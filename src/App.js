6ae8b466ce3c9ad95cef32fde153c71f5f265cd0
Gamas02 <gamaliermatheus4@gmail.com>
2025-09-04 20:32:32 -0300

    NEW: o controle de estados foi passado para tabuleiro

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import { useState } from 'react';
 import './App.css';
 
-function Square(){
-  const [valor, setValor] = useState(null)
-  function handleClick(){
-    setValor('X');
-  }
+function Square({valor, onSquareClick}){
   return(
-    <button classname="square" onClick={handleClick}>
+    <button classname="square" onClick={onSquareClick}>
       {valor}
     </button>
   );
 }
 
 export default function Tabuleiro(){
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleCklick(i){
+    const nextSqueres = squares.slice();
+    nextSqueres[i] = 'X';
+    setSquares(nextSqueres);
+  }
+
   return(
     <div>
       <div>
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square valor={squares[0]} onSquareClick={() => handleCklick(0)}/>
+        <Square valor={squares[1]} onSquareClick={() => handleCklick(1)}/>
+        <Square valor={squares[2]} onSquareClick={() => handleCklick(2)}/>
       </div>
       <div>
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square valor={squares[3]} onSquareClick={() => handleCklick(3)}/>
+        <Square valor={squares[4]} onSquareClick={() => handleCklick(4)}/>
+        <Square valor={squares[5]} onSquareClick={() => handleCklick(5)}/>
       </div>
       <div>
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square valor={squares[6]} onSquareClick={() => handleCklick(6)}/>
+        <Square valor={squares[7]} onSquareClick={() => handleCklick(7)}/>
+        <Square valor={squares[8]} onSquareClick={() => handleCklick(8)}/>
       </div>
     </div>
   );
