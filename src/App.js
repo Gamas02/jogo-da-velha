cb87c09b2aa7925e4d28948f655c4536752c2373
Gamas02 <gamaliermatheus4@gmail.com>
2025-09-04 21:47:43 -0300

    NEW: logicar para finalizar jogo com vencedor

--- src/App.js
+++ src/App.js
@@ -15,16 +15,14 @@ export default function Tabuleiro(){
 
   function handleCklick(i){
     const nextSqueres = squares.slice();
-    if(nextSqueres[i]){
+    if(nextSqueres[i] || calculaVencedor(squares)){
       return;
     }
     if (xIsNext){
       nextSqueres[i] = 'X';
-      setXIsNext(false)
     }
     else{
       nextSqueres[i] = 'O';
-      setXIsNext(true)
     }
     setXIsNext(!xIsNext);
     setSquares(nextSqueres);
@@ -49,4 +47,18 @@ export default function Tabuleiro(){
       </div>
     </div>
   );
+}
+
+function calculaVencedor(squares){
+  const lines = ([0, 1, 2], [3, 4, 5], [6, 7, 8], 
+                [0, 3, 6], [1, 4, 7], [2, 5, 8],
+              [0, 4, 8], [2, 4, 6]);
+
+  for(let i = 0; i<lines.length; i++){
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a]
+    }
+  }
+  return null;
 }
